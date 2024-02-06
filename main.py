0cd109928fd6f5bc5d75ca7bb0b2f39a8632e05e
arshemii <arshemii1373.2@gmail.com>
2024-02-06 19:22:46 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+resume = ''
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    # Parameters to set
+    parser.add_argument('--mode',
+                        type=str,
+                        default='train',
+                        choices=['train', 'test'])
+    parser.add_argument("--root_dataset",
+                        type=str,
+                        default='./dataset/LINEMOD')
+    parser.add_argument("--batch_size",
+                        type=str,
+                        default='4')
+    parser.add_argument("--class_name",
+                        type=str,
+                        default='ape')
+    parser.add_argument("--initial_lr",
+                        type=float,
+                        default=1e-4)
+    parser.add_argument("--kpt_num",
+                        type=str,
+                        default='1')                        
+    parser.add_argument('--model_dir',
+                    type=str,
+                    default='ckpts/')   
+
+    opts = parser.parse_args()
+
+    cfg = get_config()[1]
+    opts.cfg = cfg
+
+    if opts.mode in ['train']:
+        opts.out = get_log_dir(opts.dname+'/'+opts.class_name+'Kp'+opts.kpt_num, cfg)
+        print('Output logs: ', opts.out)
+        vis = SummaryWriter(logdir=opts.out+'/tbLog/')
+    else:
+        vis = []
+
+    data = get_loader(opts)
+    
+    trainer = Trainer(data, opts, vis)
+    if opts.mode == 'test':
+        trainer.Test()
+    else:
+        trainer.Train()
